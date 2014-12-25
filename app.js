8cf37be3b3f126cfda6e43e56ac6061672d4c9a8
Sarayya, Ritesh Kumar <Ritesh.Sarayya@sungard.com>
2014-12-26 02:06:42 +0530

    Adds function stubs.
    - For reeading the file
    - For finding the data
    - For replacing the data
    - For writing in the file

---
+++ app.js
@@ -0,0 +1,11 @@
+// 1) read file
+function readInputFile(){}
+
+// 2) find data
+function findfromInputdata(){}
+
+// 3) replace data
+function replaceInputdata(){}
+
+// 4) write data
+function writeInputFile(){}
